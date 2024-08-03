1a409ddd3e26524f7e3f6863ad81a5a5b8cca771
Vinay Thakor <vinay.th.24@gmail.com>
2024-08-03 17:28:59 +0530

    Add files via upload

--- FOS/Assignment_1/q4.sh
+++ FOS/Assignment_1/q4.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-echo "Enter your basic salary: "
-read basic
+# 4. Write a script to calculate gross salary. ( DA = 60%OF BASIC, HRA = 20% OF BASIC, MA = 100 and IT = 15% of BASIC)
 
-bs=$(expr $basic)
+read -p "Enter your basic salary: " basic
 
-da=$(expr $basic \* 60 / 100)
-hra=$(expr $basic \* 20 / 100)
-it=$(expr $basic \* 15 / 100)
+da=$(echo "scale=2; $basic*60/100" | bc)
+hra=$(echo "scale=2; $basic*20/100" | bc)
 ma=100
+it=$(echo "scale=2; $basic*15/100" | bc)
 
-gross=$(expr $basic + $hra + $da + $it + $ma)
+gross_salary=$(echo "$basic + $da + $hra + $ma - $it" | bc)
 
-echo "Your gross salary came to be: $gross"
+echo "Your gross salary is $gross_salary"
