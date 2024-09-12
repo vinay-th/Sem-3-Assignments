db9349aabcea0f95a86510e0615a81e6b7fc0515
Vinay Thakor <vinay.th.24@gmail.com>
2024-09-12 18:35:53 +0530

    2d array with user

---
+++ CPP/assignment_3/6_User2DArray.cpp
@@ -0,0 +1,39 @@
+// 7. Write a C++ program to enter a 2D Array and print those elements.
+#include <iostream>
+using namespace std;
+class Array {
+public:
+  int userRow() {
+    int size;
+    cout << "Enter the size of row of the array: ";
+    cin >> size;
+    return size;
+  }
+  int userColumn() {
+    int size;
+    cout << "Enter the size of column of the array: ";
+    cin >> size;
+    return size;
+  }
+};
+int main() {
+  Array obj;
+  int row = obj.userRow();
+  int column = obj.userColumn();
+  int array[row][column];
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < column; j++) {
+      cout << "Enter the element " << i + 1 << " " << j + 1 << ": ";
+      cin >> array[i][j];
+    }
+  }
+  cout << "The elements of the array are: " << endl;
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < column; j++) {
+      cout << array[i][j] << " ";
+    }
+    cout << endl;
+  }
+  cout << "\n";
+  return 0;
+}
