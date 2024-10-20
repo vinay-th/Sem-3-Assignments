40a2b6616f67967e3f9815a28335cc94478336b7
Vinay Thakor <vinay.th.24@gmail.com>
2024-10-21 00:25:52 +0530

    doneee question 8

--- FOS/Assignment_4/q8.sh
+++ FOS/Assignment_4/q8.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 # 8. Write a shell script that compresses the folder files separetely.
-
-zip -r newZip ./folder/f1 ./folder/f2
+folder=folder
+for file in "$folder"/*; do
+    if [ -f "$file" ]; then
+        echo "Compressing $file:"
+        gzip $file
+    fi
+done
