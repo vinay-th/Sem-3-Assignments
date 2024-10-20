9a55e1cdec70f34a93622bdd940856bc21e6eb33
Vinay Thakor <vinay.th.24@gmail.com>
2024-10-20 23:28:34 +0530

    assignment 4 start

---
+++ FOS/Assignment_4/q1.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# 1. Write a shell script that display the disk usage of files and directories on a machine.
+du -a
---
+++ FOS/Assignment_4/q2.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# 2. Write a shell script that display full summary of available and used disk space usage of the file system on linux system.
+df
