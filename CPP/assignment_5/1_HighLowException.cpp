996052495e5135d512ba822c280e4dd7545bd4e2
Vinay Thakor <vinay.th.24@gmail.com>
2024-10-21 18:08:14 +0530

    High low with exception

---
+++ CPP/assignment_5/1_HighLowException.cpp
@@ -0,0 +1,40 @@
+// 1. Write a program to input n integer numbers and display highest and second
+// highest number. Also handle the different exceptions possible to be thrown
+// during execution.
+#include <exception>
+#include <iostream>
+
+using namespace std;
+int main() {
+  int n;
+  cout << "Enter the number of integers: ";
+  cin >> n;
+
+  try {
+    if (n <= 1)
+      throw 1;
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+      cout << "Enter the number on [" << i << "] index: ";
+      cin >> arr[i];
+    }
+    int max = 0;
+    int sMax = 0;
+    for (int i = 0; i < n; i++) {
+      if (arr[i] > max) {
+        sMax = max;
+        max = arr[i];
+      }
+    }
+
+    cout << "The largest number in array is: " << max << endl;
+    cout << "The second largest number in array is: " << sMax << endl;
+
+  } catch (int e) {
+    cout << "Enter more than 1 number please";
+  }
+
+  cout << "\n";
+  return 0;
+}
